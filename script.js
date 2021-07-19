1870dc581ef2a9f2fc07c3477901f8c5c6b01168
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-19 07:18:38 +0300

    Add JS

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const piano = document.querySelector(".piano");
 
+piano.addEventListener("click", (e) => {
+    if (e.target.classList.contains("white")) {
+        e.target.classList.add("white--active");
+        window.setTimeout(() => {
+            e.target.classList.remove("white--active");
+        }, 400);
+    }
+    if (e.target.classList.contains("black")) {
+        e.target.classList.add("black--active");
+        window.setTimeout(() => {
+            e.target.classList.remove("black--active");
+        }, 400);
+    }
+
+    if (e.target.classList.contains("key")) {
+        let note = e.target.dataset.note;
+
+        document.querySelector(`#${note}`).play();
+    }
+});
